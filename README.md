1547b5fa30e57486f6845fe680d862c6956b3609
Adrian Stainforth <adrian.stainforth@gmail.com>
2017-02-02 11:51:18 +0000

    init

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
 # morse-marker
-Morse Marker for all projects
+Morse Marker for JS on all projects outputs into console
+
+```javascript
+    __  ___                        ____  _       _ __        __
+   /  |/  /___  _____________     / __ \(_)___ _(_) /_____ _/ /
+  / /|_/ / __ \/ ___/ ___/ _ \   / / / / / __ `/ / __/  __`/ /
+ / /  / / /_/ / /  (__  )  __/  / /_/ / / /_/ / / /_/ /_/ / /
+/_/  /_/\____/_/  /____/\___/  /_____/_/\__, /_/\__/\__,_/_/
+                                       /____/
+
+SITE BUILT BY MORSE DIGITAL © 2017- http://morsedigital.com
+```
---
+++ index.js
@@ -0,0 +1,13 @@
+var date = new Date();
+var year = date.getFullYear();
+var morse = '    __  ___                        ____  _       _ __        __\n';
+morse += '   /  |/  /___  _____________     / __ \\(_)___ _(_) /_____ _/ /\n';
+morse += '  / /|_/ / __ \\/ ___/ ___/ _ \\   / / / / / __ \`/ / __/  __\`/ /\n';
+morse += ' / /  / / /_/ / /  (__  )  __/  / /_/ / / /_/ / / /_/ /_/ / /\n';
+morse += '/_/  /_/\\____/_/  /____/\\___/  /_____/_/\\__, /_/\\__/\\__,_/_/';
+morse += '                                       /____/\n';
+morse += 'SITE BUILT BY MORSE DIGITAL © '+ year +' - http://morsedigital.com\n'
+
+/* eslint-disable */
+console.log(morse);
+/* eslint-enable */
