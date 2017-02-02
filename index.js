72b82d857d3a0cd1eae440a2fb3d1b6948f51cd4
Adrian Stainforth <adrian.stainforth@gmail.com>
2017-02-02 12:00:12 +0000

    fix minor isse

--- index.js
+++ index.js
@@ -4,9 +4,9 @@ var morse = '    __  ___                        ____  _       _ __        __\n';
 morse += '   /  |/  /___  _____________     / __ \\(_)___ _(_) /_____ _/ /\n';
 morse += '  / /|_/ / __ \\/ ___/ ___/ _ \\   / / / / / __ \`/ / __/  __\`/ /\n';
 morse += ' / /  / / /_/ / /  (__  )  __/  / /_/ / / /_/ / / /_/ /_/ / /\n';
-morse += '/_/  /_/\\____/_/  /____/\\___/  /_____/_/\\__, /_/\\__/\\__,_/_/';
+morse += '/_/  /_/\\____/_/  /____/\\___/  /_____/_/\\__, /_/\\__/\\__,_/_/\n';
 morse += '                                       /____/\n';
-morse += 'SITE BUILT BY MORSE DIGITAL © '+ year +' - http://morsedigital.com\n'
+morse += 'SITE BUILT BY MORSE DIGITAL © '+ year +' - http://morsedigital.com\n\n'
 
 /* eslint-disable */
 console.log(morse);
